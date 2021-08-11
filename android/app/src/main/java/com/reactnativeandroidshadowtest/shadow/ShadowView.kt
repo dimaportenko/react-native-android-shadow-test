667366d34b553984e4fdb0ebf4104ccf2038168c
Dmytro Portenko <troublesohard@gmail.com>
2021-08-11 14:21:15 +0300

    cleanup

--- android/app/src/main/java/com/reactnativeandroidshadowtest/shadow/ShadowView.kt
+++ android/app/src/main/java/com/reactnativeandroidshadowtest/shadow/ShadowView.kt
@@ -15,25 +15,17 @@ class ShadowView(
     defStyleRes: Int = 0
 ) :
     ShadowLayout(context, attrs, defStyleAttr, defStyleRes) {
-    //    app:shadow_cast_only_background="true"
-//    app:shadow_color="#808"
-//    app:shadow_downscale="1"
-//    app:shadow_radius="8"
-//    app:shadow_with_color="true"
-//    app:shadow_with_content="true"
-//    app:shadow_with_css_scale="true"
-//    app:shadow_with_dpi_scale="true"
-//    app:shadow_x_shift="1dp"
-//    app:shadow_y_shift="2dp">
     init {
         shadow_color = Color.parseColor("#ffa8a8");
         shadow_with_color = true
         shadow_with_content = true
         shadow_with_css_scale = true
         shadow_with_dpi_scale = true
+        shadow_cast_only_background = true
         shadow_x_shift = 3.0f
         shadow_y_shift = 3.0f
         shadow_radius = 5.0f
+        shadow_downscale = 1f
     }
 
 }
